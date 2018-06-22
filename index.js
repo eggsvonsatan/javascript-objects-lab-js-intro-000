db8a718f9faaf5c183f3d141e496c33f9f7da2d7
eggsvonsatan <elbadry.d@gmail.com>
2018-06-22 02:32:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
   recipes[key] = value; return recipes
 }
 
+function deleteFromObjectByKey (object, key) {
+  var newRecipes = Object.assign({}, object);
+  delete newRecipes[key];
+  return newRecipes
+}
 
 function destructivelyDeleteFromObjectByKey(recipes, key){
  delete recipes[key];
